f7e7138eeb3ea342c31656e4575af9d70376d3a6
vaneena <alveenaayesha941@gmail.com>
2024-12-09 14:57:42 +0500

    Create main.java

---
+++ main.java
@@ -0,0 +1,5 @@
+public static void main(String[] args) {
+        int number = 5;
+        int square = number * number;
+        System.out.println("Square of " + number + " is: " + square);
+    }
